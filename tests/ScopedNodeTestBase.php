157bece7cf2c36a07e11189eba3b4bc49f2e01d4
Aimeos <aimeos@aimeos.org>
2026-03-06 13:39:16 +0100

    Fixed tests

--- tests/ScopedNodeTestBase.php
+++ tests/ScopedNodeTestBase.php
@@ -65,24 +65,6 @@ abstract class ScopedNodeTestBase extends \Orchestra\Testbench\TestCase
         parent::tearDown();
     }
 
-    protected function seedTable(string $table, array $data): void
-    {
-        $driver = DB::connection()->getDriverName();
-        $prefixedTable = DB::connection()->getTablePrefix() . $table;
-
-        if ($driver === 'sqlsrv') {
-            DB::statement("SET IDENTITY_INSERT [$prefixedTable] ON");
-        }
-
-        DB::table($table)->insert($data);
-
-        if ($driver === 'sqlsrv') {
-            DB::statement("SET IDENTITY_INSERT [$prefixedTable] OFF");
-        } elseif ($driver === 'pgsql') {
-            DB::statement("SELECT setval(pg_get_serial_sequence('$prefixedTable', 'id'), coalesce(max(id), 0) + 1, false) FROM \"$prefixedTable\"");
-        }
-    }
-
     protected function refreshIds(string $table, array $ids): array
     {
         $dbIds = DB::table($table)->pluck('id')->all();
